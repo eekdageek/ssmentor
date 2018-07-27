91e218f41ce88f2b35e8a0b8758e2ba0b1c161ff
James Fairbairn <james@netlagoon.com>
2018-07-26 22:21:56 -0700

    remove csrf protection

--- web/app/controllers/checkins_controller.rb
+++ web/app/controllers/checkins_controller.rb
@@ -1,5 +1,5 @@
 class CheckinsController < ActionController::API
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
   def update
     @checkin = Checkin.find(params[:id])
     body = request.body.read
