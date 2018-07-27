3d66fa13d3bca2ed11f3e656f0d001d9037d0c81
jloeb <Jess.Loeb@sony.com>
2018-07-26 23:23:00 -0700

    slackathonnnnn

--- node-app/src/index.js
+++ node-app/src/index.js
@@ -81,6 +81,7 @@ app.post('/interaction/:slackId', urlencodedParser,
     let userInfo;
     slackApi.users.info({ user: user })
     .then(resp => {
+      console.log('RESPONSE: ',resp);
       if (resp.user) {
         userInfo= resp.user[0];
       } else {
